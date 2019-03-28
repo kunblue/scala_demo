7d2e17664ca15e926decf7be0a62583bd2cf079f
lankun <lankun1234@163.com>
2019-03-28 16:53:28 +0800

    test case class

--- src/main/scala/com/lankun/demo/Message.scala
+++ src/main/scala/com/lankun/demo/Message.scala
@@ -4,19 +4,19 @@ class Message {
   val timeStamp = new java.util.Date()
 }
 
-sealed abstract class Person
+sealed abstract class Person1
 
-case class Student(name: String, age: Int, studentNo: Int) extends Person
+case class Student(name: String, age: Int, studentNo: Int) extends Person1
 
-case class Teacher(name: String, age: Int, teacherNo: Int) extends Person
+case class Teacher(name: String, age: Int, teacherNo: Int) extends Person1
 
-case class Nobody(name: String) extends Person
+case class Nobody(name: String) extends Person1
 
-case class SchoolClass(classDescription: String, persons: Person*)
+case class SchoolClass(classDescription: String, persons: Person1*)
 
 object CaseClassDemo {
   def main(args: Array[String]): Unit = {
-    val s: Person = Student("john", 18, 1024)
+    val s: Person1 = Student("john", 18, 1024)
     //这边仅仅给出了匹配Student的情况，在编译时
     //编译器会提示
     //match may not be exhaustive. It would fail on the following inputs: Nobody(_), Teacher(_, _, _)
