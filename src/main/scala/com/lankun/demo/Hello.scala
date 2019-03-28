b001f136e8441053174bc2d1b67a948038edf4bd
lankun <lankun1234@163.com>
2019-03-28 16:46:27 +0800

    test case class

--- src/main/scala/com/lankun/demo/Hello.scala
+++ src/main/scala/com/lankun/demo/Hello.scala
@@ -7,3 +7,37 @@ object Hello extends App {
     println("Hello world!")
   }
 }
+
+object PatternMatching extends App {
+  for (i <- 1 to 100) {
+    i match {
+      case 10 => println(10)
+      case 50 => println(50)
+      case 80 => println(80)
+      case _ =>
+    }
+  }
+}
+
+
+abstract class Person
+
+case class Student(name: String, age: Int, studentNo: Int) extends Person
+
+case class Teacher(name: String, age: Int, teacherNo: Int) extends Person
+
+case class Nobody(name: String) extends Person
+
+//SchoolClass为接受多个Person类型参数的类
+case class SchoolClass(classDescription: String, persons: Person*)
+
+//下列代码给出的是其模式匹配应用示例
+object CaseClassDemo {
+  def main(args: Array[String]): Unit = {
+    val sc = SchoolClass("学途无忧网Scala培训班", Teacher("摇摆少年梦T", 27, 2015), Student("摇摆少年梦", 27, 2015))
+    sc match {
+      case SchoolClass(_, Teacher(name, age, teacherNo), Student(name1, age1, studentNo)) => println(name + "-" + age + "-" + studentNo)
+      case _ => println("Nobody")
+    }
+  }
+}
--- src/main/scala/com/lankun/demo/Message.scala
+++ src/main/scala/com/lankun/demo/Message.scala
@@ -3,3 +3,25 @@ package com.lankun.demo
 class Message {
   val timeStamp = new java.util.Date()
 }
+
+sealed abstract class Person
+
+case class Student(name: String, age: Int, studentNo: Int) extends Person
+
+case class Teacher(name: String, age: Int, teacherNo: Int) extends Person
+
+case class Nobody(name: String) extends Person
+
+case class SchoolClass(classDescription: String, persons: Person*)
+
+object CaseClassDemo {
+  def main(args: Array[String]): Unit = {
+    val s: Person = Student("john", 18, 1024)
+    //这边仅仅给出了匹配Student的情况，在编译时
+    //编译器会提示
+    //match may not be exhaustive. It would fail on the following inputs: Nobody(_), Teacher(_, _, _)
+    s match {
+      case Student(name, age, studentNo) => println("Student")
+    }
+  }
+}
